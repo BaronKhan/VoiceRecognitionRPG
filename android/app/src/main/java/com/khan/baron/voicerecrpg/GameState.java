5b67657621b8b918987e27a335f374f7242f2b28
BaronKhan <bksonic@hotmail.co.uk>
2017-11-27 16:37:33 +0000

    GameState.java: output synonyms and hypernyms

--- android/app/src/main/java/com/khan/baron/voicerecrpg/GameState.java
+++ android/app/src/main/java/com/khan/baron/voicerecrpg/GameState.java
@@ -2,11 +2,17 @@ package com.khan.baron.voicerecrpg;
 
 import android.app.Activity;
 
+import java.util.Iterator;
+import java.util.List;
+
 import edu.mit.jwi.IDictionary;
 import edu.mit.jwi.item.IIndexWord;
+import edu.mit.jwi.item.ISynset;
+import edu.mit.jwi.item.ISynsetID;
 import edu.mit.jwi.item.IWord;
 import edu.mit.jwi.item.IWordID;
 import edu.mit.jwi.item.POS;
+import edu.mit.jwi.item.Pointer;
 
 public class GameState {
     public Activity mMainActivity;
@@ -19,12 +25,48 @@ public class GameState {
 
     public String updateState(String input) {
         // get nouns
-
-
-        IIndexWord idxWord = mDict.getIndexWord ("dog", POS.NOUN );
+        IIndexWord idxWord = mDict.getIndexWord (input, POS.NOUN );
         IWordID wordID = idxWord.getWordIDs().get(0);
         IWord word = mDict.getWord(wordID);
-        return word.getSynset().getGloss ();
+        ISynset synset = word.getSynset();
+
+        // get synonyms
+        String output = "synonyms: \n";
+        for (IWord w : synset.getWords()) {
+            output += w.getLemma() + ", ";
+        }
+
+        //get hypernyms
+        output += "\n\nhypernyms: \n";
+        List<ISynsetID> hypernymsList = synset.getRelatedSynsets(Pointer.HYPERNYM);
+        for (ISynsetID sid : hypernymsList) {
+            List <IWord > words = mDict.getSynset(sid).getWords();
+            output += "{";
+            for (Iterator<IWord> i = words.iterator(); i.hasNext();) {
+                output += i.next().getLemma() + ", ";
+            }
+            output += "}, ";
+        }
+
+
+        // print out the first hypernym branch of the word
+        output += "\n\nfirst hypernym branch: \n";
+        String currentWord = input;
+        for (int i=0; i<5; ++i) {
+            IIndexWord idxWord2 = mDict.getIndexWord (currentWord, POS.NOUN );
+            IWordID wordID2 = idxWord2.getWordIDs().get(0);
+            IWord word2 = mDict.getWord(wordID2);
+            ISynset synset2 = word2.getSynset();
+            List<ISynsetID> hypernymsList2 = synset2.getRelatedSynsets(Pointer.HYPERNYM);
+            List<IWord> words = mDict.getSynset(hypernymsList2.get(0)).getWords();
+            if (words.size() > 0) {
+                currentWord = words.get(0).getLemma();
+                output += currentWord + " --> ";
+            } else {
+                break;
+            }
+        }
+        return output;
     }
 
 }
