5ce8614c530b4db07504b3de31150b3b1fd0a30c
BaronKhan <bksonic@hotmail.co.uk>
2018-05-15 12:42:38 +0100

    Update buildJar.sh

--- library/buildJar.sh
+++ library/buildJar.sh
@@ -8,7 +8,7 @@ LIBPOS="$LIBDIR/stanford-postagger.jar"
 LIBWS4J="$LIBDIR/ws4j-1.0.1.jar"
 
 if [ ! -d "../android" ]; then
-  echo "execute this script in the build dir"
+  echo "execute this script in the library/ dir"
   exit 0
 fi
 
@@ -30,18 +30,18 @@ do
     sed -i '/+"= "+totalScore);/d' ./$FILE
     sed -i '/android.os.Environment;/d' ./$FILE
     sed -i '/android.app.Activity;/d' ./$FILE
-    sed -i 's/android.util.Pair/com.khan.baron.voicerecrpg.system.Pair/g' ./$FILE
-    sed -i 's/android.util.Triple/com.khan.baron.voicerecrpg.system.Triple/g' ./$FILE
+    sed -i 's/android.util.Pair/com.khan.baron.vcw.Pair/g' ./$FILE
+    sed -i 's/android.util.Triple/com.khan.baron.vcw.Triple/g' ./$FILE
     sed -i '/Environment.getExternalStorageDirectory()/d' ./$FILE
     sed -i 's/+ "\/english-left3words-distsim.tagger";/String modelPath="english-left3words-distsim.tagger";/g' ./$FILE
+    sed -i 's/voicerecrpg\/system/vcw/g' ./$FILE
+    sed -i 's/voicerecrpg.system/vcw/g' ./$FILE
   fi
 done
 
-printf "package com.khan.baron.voicerecrpg.system; public class Pair<F, S> { public final F first; public final S second; public Pair(F first, S second) { this.first=first; this.second=second; } }" > Pair.java
-printf "package com.khan.baron.voicerecrpg.system; public class Triple<F, S, T> { public final F first; public final S second; public final T third; public Triple(F first, S second, T third) { this.first=first; this.second=second; this.third=third; } }" > Triple.java
+printf "package com.khan.baron.vcw; public class Pair<F, S> { public final F first; public final S second; public Pair(F first, S second) { this.first=first; this.second=second; } }" > Pair.java
+printf "package com.khan.baron.vcw; public class Triple<F, S, T> { public final F first; public final S second; public final T third; public Triple(F first, S second, T third) { this.first=first; this.second=second; this.third=third; } }" > Triple.java
 
-javac -cp ".;$LIBCOMON;$LIBJWI;$LIBPOS;$LIBWS4J" *.java
-
-jar cvf vcw.0.0.1.jar *.class
+javac -cp ".;$LIBCOMON;$LIBJWI;$LIBPOS;$LIBWS4J" *.java && jar cvf vcw.0.0.1.jar *.class
 
 rm -Rf *.java *.class
