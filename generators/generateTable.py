92cad61cc629fdd501999f310700ba0ec4be9ad7
BaronKhan <bksonic@hotmail.co.uk>
2018-05-23 19:43:57 +0100

    Add generateTable.py

---
+++ generators/generateTable.py
@@ -0,0 +1,74 @@
+import sys
+import os.path
+import csv
+
+def renderBeginningFile(outputFile, outputName):
+    outputFile.write("\
+import com.khan.baron.vcw.ContextActionMap;\n\
+import com.khan.baron.vcw.GlobalState;\n\
+import cooking.actions.*;\n\
+\n\
+public class "+outputName+" extends ContextActionMap {\n\
+    public "+outputName+"(GlobalState state) {\n\
+        super(state);\n"
+    )
+
+def renderEndFile(outputFile):
+    outputFile.write("\
+    }\n\
+}"
+    )
+
+def renderJava(tableReader, outputName):
+    outputFile = open(outputName+".java", 'w')
+
+    renderBeginningFile(outputFile, outputName)
+
+    rowNum = 0
+    numActions = 0
+    for row in tableReader:
+        if rowNum < 2:
+            if rowNum < 1:
+                numActions = len(row)-1
+                outputFile.write("        setActionList(")
+            else:
+                outputFile.write("        addDefaultContextActions(")
+        else:
+            outputFile.write("        addContextActions(\""+row[0]+"\", ")
+
+        rowStr = ""
+        for i in range(1, max(len(row), numActions+1)):
+            if i >= len(row):
+                rowStr += "null"
+            else:
+                rowStr += str(row[i])
+            if i != max(len(row), numActions+1)-1:
+                rowStr += ", "
+        rowStr += ");\n"
+        outputFile.write(rowStr)
+
+        rowNum += 1
+
+    renderEndFile(outputFile)
+
+    outputFile.close()
+
+
+if len(sys.argv) < 3:
+    print("usage: python generateTable.py <csv-path> <output-file-without-.java>")
+    exit(0)
+
+if ".java" in sys.argv[2]:
+    print("error: remove .java extension from file name")
+    exit(0)
+
+fileName = sys.argv[1]
+outputName = sys.argv[2]
+
+if not os.path.isfile(fileName):
+    print("error: "+fileName+" does not exist")
+    exit(0)
+
+with open(fileName, 'rt') as f:
+    tableReader = csv.reader(f)
+    renderJava(tableReader, outputName)
