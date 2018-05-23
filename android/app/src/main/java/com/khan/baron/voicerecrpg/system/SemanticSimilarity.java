654df50b8020199406d135f50e8f7943ec268cbe
BaronKhan <bksonic@hotmail.co.uk>
2018-05-23 01:53:42 +0100

    Update final report

--- android/app/src/main/java/com/khan/baron/voicerecrpg/system/SemanticSimilarity.java
+++ android/app/src/main/java/com/khan/baron/voicerecrpg/system/SemanticSimilarity.java
@@ -115,11 +115,8 @@ public class SemanticSimilarity {
             if (sCurrentMethod1 == SimilarityMethod.METHOD_LESK) { score1 = max(score1 / 80.0, 1.0); }
             if (sCurrentMethod2 == SimilarityMethod.METHOD_LESK) { score2 = max(score2 / 80.0, 1.0); }
 
-            if (score2 > 0) {
-                score = (score1 * 0.5) + (score2 * 0.5);
-            } else {
-                score = score1;
-            }
+            if (score2 > 0) { score = (score1 * 0.5) + (score2 * 0.5); }
+            else { score = score1; }
 
             Log.d("SemanticSimilarity", "score("+word1+", "+word2+") = "+score);
 
