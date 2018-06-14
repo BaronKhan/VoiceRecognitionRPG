eb7c23ca2e1f592736b0c5648824c4905545f3f0
BaronKhan <bksonic@hotmail.co.uk>
2018-06-14 01:29:45 +0100

    Assign properties to weapons with engine

--- android/app/src/main/java/com/khan/baron/voicerecrpg/game/items/Weapon.java
+++ android/app/src/main/java/com/khan/baron/voicerecrpg/game/items/Weapon.java
@@ -1,11 +1,15 @@
 package com.khan.baron.voicerecrpg.game.items;
 
+import com.khan.baron.voicerecrpg.system.SemanticSimilarity;
+
 /**
  * Created by Baron on 31/01/2018.
  */
 
 public class Weapon extends Item {
     public double mDamageModifier;
+
+    private final double PROPERTY_THRESHOLD=0.85;
 
     public Weapon(String name) {
         super (name, ItemType.ITEM_WEAPON);
@@ -19,9 +23,13 @@ public class Weapon extends Item {
     public Weapon(String name, double damageMod, String ... description) {
         super (name, ItemType.ITEM_WEAPON, description);
         mDamageModifier = damageMod;
-        if (getDescription().contains("sharp")) {
+        if (getDescription().contains("sharp") ||
+                SemanticSimilarity.getInstance().calculateScore(mName, "sharp") > PROPERTY_THRESHOLD)
+        {
             setContext("weapon-sharp");
-        } else if (getDescription().contains("blunt")) {
+        } else if (getDescription().contains("blunt") ||
+                SemanticSimilarity.getInstance().calculateScore(mName, "blunt") > PROPERTY_THRESHOLD)
+        {
             setContext("weapon-blunt");
         } else {
             setContext("weapon");
