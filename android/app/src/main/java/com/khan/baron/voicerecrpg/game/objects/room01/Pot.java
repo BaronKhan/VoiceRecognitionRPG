27a7381c733f2ed8b56a079076a8443c6a201a36
BaronKhan <bksonic@hotmail.co.uk>
2018-06-18 01:31:03 +0100

    Add Pot to game

---
+++ android/app/src/main/java/com/khan/baron/voicerecrpg/game/objects/room01/Pot.java
@@ -0,0 +1,40 @@
+package com.khan.baron.voicerecrpg.game.objects.room01;
+
+import com.khan.baron.voicerecrpg.game.GameState;
+import com.khan.baron.voicerecrpg.game.actions.sharedActions.ShowInventory;
+import com.khan.baron.voicerecrpg.game.items.Item;
+import com.khan.baron.voicerecrpg.game.objects.PhysicalObject;
+import com.khan.baron.voicerecrpg.system.Entity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Pot extends PhysicalObject {
+    private List<Item> mItems = new ArrayList<>();
+
+    public Pot(Item ... items) {
+        super("pot", "jar", "vase");
+        setContext("pot");
+        setBreakable(true);
+        setScratchable(true);
+        setCuttable(false);
+        mItems.addAll(Arrays.asList(items));
+    }
+
+    @Override
+    public String onBroken(GameState gameState, Entity currentContext) {
+        gameState.actionSucceeded();
+        String output = "You broke the pot. ";
+        if (mItems.size() > 0) {
+            output += "The following items were inside:\n";
+            for (Item item : mItems) {
+                gameState.getInventory().add(item);
+                output += " - "+item.getName();
+            }
+            output += "All items were added to inventory.\n"+
+                    (new ShowInventory().execute(gameState, gameState.getInventory()));
+        }
+        return output;
+    }
+}
